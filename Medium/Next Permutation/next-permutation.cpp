4df0c2a57bd7c4193e1ada031f40281a6de82422
Anubhavverma9918 <130591157+Anubhavverma9918@users.noreply.github.com>
2024-04-23 10:17:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Next Permutation/next-permutation.cpp
+++ Medium/Next Permutation/next-permutation.cpp
@@ -11,7 +11,24 @@ class Solution{
 public:
     vector<int> nextPermutation(int N, vector<int> arr){
         // code here
-        next_permutation(arr.begin(), arr.end());
+        int ind = -1;
+        for(int i=N-2; i>=0; i--){
+            if(arr[i] < arr[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1){
+            reverse(arr.begin(), arr.end());
+            return arr;
+        }
+        for(int i=N-1; i>ind; i--){
+            if(arr[i] > arr[ind]){
+                swap(arr[i], arr[ind]);
+                break;
+            }
+        }
+        reverse(arr.begin() + ind + 1, arr.end());
         return arr;
     }
 };
