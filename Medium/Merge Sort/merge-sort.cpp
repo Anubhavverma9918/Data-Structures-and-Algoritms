bd2b0ef7fe48413f1590936c6b695960563b3693
Anubhavverma9918 <130591157+Anubhavverma9918@users.noreply.github.com>
2024-04-24 22:21:03 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort/merge-sort.cpp
+++ Medium/Merge Sort/merge-sort.cpp
@@ -21,43 +21,63 @@ class Solution
     public:
     void merge(int arr[], int l, int m, int r)
     {
-         // Your code here
-         vector<int> temp;
-         int left = l;
-         int right = m+1;
-         while(left<=m && right<=r){
-            if(arr[left] <= arr[right]){
-                temp.push_back(arr[left]);
-                left++;
+        int an = m - l + 1;
+        int bn = r - m;
+
+        // Create two arrays
+        int a[an];
+        int b[bn];
+
+        // Copy the values
+        for (int i = 0; i < an; i++)
+        {
+            a[i] = arr[l + i];
+        }
+
+        for (int j = 0; j < bn; j++)
+        {
+            b[j] = arr[m + 1 + j];
+        }
+
+        // Merge two sorted arrays
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < an && j < bn)
+        {
+            if (a[i] < b[j])
+            {
+                arr[k++] = a[i++];
             }
-            else{
-                temp.push_back(arr[right]);
-                right++;
+            else
+            {
+                arr[k++] = b[j++];
             }
-         }
-         while(left<=m){
-             temp.push_back(arr[left]);
-             left++;
-         }
-         while(right<=r){
-             temp.push_back(arr[right]);
-             right++;
-         }
-         for(int i=l; i<=r; i++){
-             arr[i] = temp[i-l];
-         }
+        }
+
+        // Copy the remaining elements of a[] if any
+        while (i < an)
+        {
+            arr[k++] = a[i++];
+        }
+
+        // Copy the remaining elements of b[] if any
+        while (j < bn)
+        {
+            arr[k++] = b[j++];
+        }
     }
-    public:
+
+public:
     void mergeSort(int arr[], int l, int r)
     {
-        //code here
-        if(l==r){
+        if (l >= r)
             return;
-        }
-        int mid = (l+r)/2;
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+
+        int mid = l + (r - l) / 2; // Fixed the calculation of mid
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
 };
 
