3659ba2d8404de9bd7c7e87555058cf8f76427a6
Anubhavverma9918 <130591157+Anubhavverma9918@users.noreply.github.com>
2024-06-04 22:53:40 +0530

    Updated Solution - GfG to GitHub

--- Easy/Stack using two queues/stack-using-two-queues.cpp
+++ Easy/Stack using two queues/stack-using-two-queues.cpp
@@ -57,23 +57,22 @@ public:
 void QueueStack :: push(int x)
 {
         // Your Code
+        q2.push(x);
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
+        swap(q1, q2);
 }
 
 //Function to pop an element from stack using two queues. 
 int QueueStack :: pop()
 {
-        // Your Code  
-        if(q1.empty()) return -1;
-        int t=q1.front();
+        // Your Code
+        if(q1.empty()){
+            return -1;
+        }
+        int top = q1.front();
         q1.pop();
-        return t;
+        return top;
 }
