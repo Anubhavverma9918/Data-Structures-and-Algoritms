01e1ab13484e7162df1e6ff63c33cfab106a1b16
Anubhav  Verma <130591157+Anubhavverma9918@users.noreply.github.com>
2024-06-05 21:39:14 +0530

    Updated Solution - GfG to GitHub

--- Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
+++ Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -40,24 +40,23 @@ struct Node
 class Solution
 {
     public:
+    void rightView(Node* root, vector<int> &ans, int level){
+        if(root == NULL){
+            return;
+        }
+        if(ans.size() == level){
+            ans.push_back(root->data);
+        }
+        rightView(root->right, ans, level+1);
+        rightView(root->left, ans, level+1);
+    }
     //Function to return list containing elements of right view of binary tree.
     vector<int> rightView(Node *root)
     {
        // Your Code here
-       vector<int> res;
-       recursion(root, 0, res);
-       return res;
-       
-    }
-    void recursion(Node* root, int level, vector<int>& res){
-        if(root == NULL){
-            return;
-        }
-        if(res.size() == level){
-            res.push_back(root->data);
-        }
-        recursion(root->right, level+1, res);
-        recursion(root->left, level+1, res);
+       vector<int> ans;
+       rightView(root, ans, 0);
+       return ans;
     }
 };
 
