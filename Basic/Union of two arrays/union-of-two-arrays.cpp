ca62113984eea6cbe4e3748c32eea52f5011ec06
Anubhav  Verma <130591157+Anubhavverma9918@users.noreply.github.com>
2024-06-15 16:46:04 +0530

    Updated Solution - GfG to GitHub

--- Basic/Union of two arrays/union-of-two-arrays.cpp
+++ Basic/Union of two arrays/union-of-two-arrays.cpp
@@ -12,15 +12,19 @@ class Solution{
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
         //code here
-        map<int,int>mp;
-    
-        for(int i =0;i<n;i++){
+        map<int, int> mp;
+        vector<int> uni;
+        
+        for(int i=0; i<n; i++){
             mp[a[i]]++;
         }
-         for(int i =0;i<m;i++){
+        for(int i=0; i<m; i++){
             mp[b[i]]++;
         }
-        return mp.size();
+        for(auto it : mp){
+            uni.push_back(it.first);
+        }
+        return uni.size();
     }
 };
 
